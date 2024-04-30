05486f93fd5424527685768960cdf0546c275d2b
AmbivalentLeather <144703073+AmbivalentLeather@users.noreply.github.com>
2024-04-29 23:57:39 -0400

    Added evaluator

---
+++ evaluator.py
@@ -0,0 +1,11 @@
+def evaluate(node):
+    if node.left is None or node.right is None:
+        return int(node.value)
+
+    left = evaluate(node.left)
+    right = evaluate(node.right)
+    match node.value:
+        case "-": return left - right
+        case "/": return left / right
+        case "+": return left + right
+        case "*": return left * right
